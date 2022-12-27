80bb65309902163031f5fc32221fbdebe6096226
Njthai <naiarajudith@gmail.com>
2022-12-27 17:53:38 +0100

    cards ready

--- src/App.js
+++ src/App.js
@@ -1,65 +1,26 @@
 import "./App.css";
+import CardsContainer from "./components/CardsContainer";
 import CardWrapper from "./components/CardWrapper";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar, faEnvelope, faCircleCheck, faDollarSign, faAddressBook, faPhoneFlip } from "@fortawesome/free-solid-svg-icons";
-import Foto1 from "./components/imagenes/Foto1.png";
+import CardImg from "./components/CardImg";
+import CardInfo from "./components/CardInfo";
+import { freelancers } from "./db/freelancers";
+
 function App() {
-  return ( 
+  console.log(freelancers);
 
-
-  <div className="card-container">
-    <CardWrapper>
-      <div className="img-container">
-        <div className="img-wrapper">
-          <img src={Foto1} alt="freelancer-one"></img>
-          <span className="dot"></span>
-        </div>
-      </div>
-      <div className="info-container">
-        <h3>MR ROBERT</h3>
-        <p>UI/UX Designer</p>
-        <div className="hire">
-          <div>
-            Rate: <span className="price">$85/hr</span>
-          </div>
-          <button className="hire-button">Hire me</button>
-        </div>
-        <div className="rating">
-        <div>5.0</div>
-        <div>
-        <FontAwesomeIcon icon={faStar} />
-        <FontAwesomeIcon icon={faStar} />
-        <FontAwesomeIcon icon={faStar} />
-        <FontAwesomeIcon icon={faStar} />
-        <FontAwesomeIcon icon={faStar} />
-        </div><div> (105 Reviews)</div>
-        </div>
-        </div>
-        <div className="icons">
-          <div className="verified">
-            <FontAwesomeIcon icon={faCircleCheck} />
-            <span>Verified</span>
-            </div>
-            <span className="circle available">
-            <FontAwesomeIcon icon={faDollarSign} />
-            </span>
-            <span className="circle available">
-            <FontAwesomeIcon icon={faEnvelope} />
-            </span>
-            <span className="circle not-available">
-            <FontAwesomeIcon icon={faAddressBook} />
-            </span>
-            <span className="circle not-available">
-            <FontAwesomeIcon icon={faPhoneFlip} />
-            </span>
-      
-      </div>
-
-    </CardWrapper>
-    </div>
-  
-  )
-
+  return (
+    <CardsContainer>
+      {freelancers.map((freelancer,index)=>{
+return(
+   <CardWrapper key={freelancer.id}>
+        <CardImg freelancer={freelancer}></CardImg>
+        <CardInfo freelancer={freelancer}></CardInfo>
+      </CardWrapper>
+)
+        })}
+     
+    </CardsContainer>
+  );
 }
 
 export default App;
---
+++ src/db/freelancers.js
@@ -0,0 +1,113 @@
+import Foto1 from "../components/imagenes/Foto1.jpg";
+import Foto2 from "../components/imagenes/Foto2.jpg";
+import Foto3 from "../components/imagenes/Foto3.jpg";
+import Foto4 from "../components/imagenes/Foto4.jpg";
+import Foto5 from "../components/imagenes/Foto5.jpg";
+import Foto6 from "../components/imagenes/Foto6.jpg";
+import Foto7 from "../components/imagenes/Foto7.jpg";
+import Foto8 from "../components/imagenes/Foto8.jpg";
+import Foto9 from "../components/imagenes/Foto9.jpg";
+import Foto10 from "../components/imagenes/Foto10.jpg";
+import Foto11 from "../components/imagenes/Foto11.jpg";
+import Foto12 from "../components/imagenes/Foto12.jpg";
+
+export const freelancers = [
+    {
+        "id":0,
+        "img":{"path":Foto1, "text":"Foto 1"},
+        "title":"Mr.",
+        "last_name":"ROBERT",
+        "role":"UI/UX Designer",
+        "fee":85,
+        "reviews":105
+    },
+    {
+        "id": 1,
+        "img":{"path":Foto2, "text":"Foto 2"},
+        "title": "Mrs.",
+        "last_name": "Malam",
+        "role": "Staff Scientist",
+        "fee": 74,
+        "reviews": 106
+      }, {
+        "id": 2,
+        "img":{"path":Foto3, "text":"Foto 3"},
+        "title": "Honorable",
+        "last_name": "O'dell",
+        "role": "Sales Associate",
+        "fee": 85,
+        "reviews": 83
+      }, {
+        "id": 3,
+        "img":{"path":Foto4, "text":"Foto 4"},
+        "title": "Mrs.",
+        "last_name": "Kinsey",
+        "role": "Director of Sales",
+        "fee": 68,
+        "reviews": 104
+      }, {
+        "id": 4,
+        "img":{"path":Foto5, "text":"Foto 5"},
+        "title": "Honorable",
+        "last_name": "Basek",
+        "role": "Engineer III",
+        "fee": 83,
+        "reviews": 113
+      }, {
+        "id": 5,
+        "img":{"path":Foto6, "text":"Foto 6"},
+        "title": "Mr.",
+        "last_name": "Grisdale",
+        "role": "VP Marketing",
+        "fee": 68,
+        "reviews": 111
+      }, {
+        "id": 6,
+        "img":{"path":Foto7, "text":"Foto 7"},
+        "title": "Rev.",
+        "last_name": "Ioselevich",
+        "role": "Health Coach II",
+        "fee": 78,
+        "reviews": 107
+      }, {
+        "id": 7,
+        "img":{"path":Foto8, "text":"Foto 8"},
+        "title": "Mr.",
+        "last_name": "Vandrill",
+        "role": "Civil Engineer",
+        "fee": 73,
+        "reviews": 90
+      }, {
+        "id": 8,
+        "img":{"path":Foto9, "text":"Foto 9"},
+        "title": "Ms.",
+        "last_name": "Ducker",
+        "role": "Quality Engineer",
+        "fee": 80,
+        "reviews": 76
+      }, {
+        "id": 9,
+        "img":{"path":Foto10, "text":"Foto 10"},
+        "title": "Rev.",
+        "last_name": "Mollin",
+        "role": "VP Accounting",
+        "fee": 82,
+        "reviews": 98
+      }, {
+        "id": 10,
+        "img":{"path":Foto11, "text":"Foto 11"},
+        "title": "Mr.",
+        "last_name": "Overstall",
+        "role": "Programmer I",
+        "fee": 69,
+        "reviews": 108
+      }, {
+        "id": 11,
+        "img":{"path":Foto12, "text":"Foto 12"},
+        "title": "Rev.",
+        "last_name": "McAuliffe",
+        "role": "Web Designer I",
+        "fee": 78,
+        "reviews": 99
+      }
+]
