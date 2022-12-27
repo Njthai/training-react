39c25f07fd3ac03df0ffccf9507d11343242e10a
Njthai <naiarajudith@gmail.com>
2022-12-27 01:28:08 +0100

    css ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import './App.css';
-import Foto1 from './components/imagenes/Foto1.png';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faCircleCheck} from '@fortawesome/free-solid-svg-icons';
-import { faCircle } from '@fortawesome/free-solid-svg-icons';
-
+import "./App.css";
+import CardWrapper from "./components/CardWrapper";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar, faEnvelope, faCircleCheck, faDollarSign, faAddressBook, faPhoneFlip } from "@fortawesome/free-solid-svg-icons";
+import Foto1 from "./components/imagenes/Foto1.png";
 function App() {
-  return (
-    <div class="cards">
-      <h1 class="title">MR. ROBERT</h1>
-      <p class="subt">UI/UX Designer</p>
-      <p class="solid">Verified</p>
-      <div>
-        <FontAwesomeIcon icon={faCircleCheck}/>{" "}
-        <FontAwesomeIcon icon={faCircle}/>
-     </div>
-      <img class="foto1" src={Foto1} alt=''/>
+  return ( 
+
+
+
+  <div className="card-container">
+    <CardWrapper>
+      <div className="img-container">
+        <div className="img-wrapper">
+          <img src={Foto1} alt="freelancer-one"></img>
+          <span className="dot"></span>
+        </div>
       </div>
-   
-  );
+      <div className="info-container">
+        <h3>MR ROBERT</h3>
+        <p>UI/UX Designer</p>
+        <div className="hire">
+          <div>
+            Rate: <span className="price">$85/hr</span>
+          </div>
+          <button className="hire-button">Hire me</button>
+        </div>
+        <div className="rating">
+        <div>5.0</div>
+        <div>
+        <FontAwesomeIcon icon={faStar} />
+        <FontAwesomeIcon icon={faStar} />
+        <FontAwesomeIcon icon={faStar} />
+        <FontAwesomeIcon icon={faStar} />
+        <FontAwesomeIcon icon={faStar} />
+        </div> <div> (105 Reviews)</div>
+        </div>
+        </div>
+        <div className="icons">
+          <div className="verified">
+            <FontAwesomeIcon icon={faCircleCheck} />
+            <span>Verified</span>
+            </div>
+            <span className="circle available">
+            <FontAwesomeIcon icon={faDollarSign} />
+            </span>
+            <span className="circle available">
+            <FontAwesomeIcon icon={faEnvelope} />
+            </span>
+            <span className="circle not-available">
+            <FontAwesomeIcon icon={faAddressBook} />
+            </span>
+            <span className="circle not-available">
+            <FontAwesomeIcon icon={faPhoneFlip} />
+            </span>
+      
+      </div>
+
+    </CardWrapper>
+    </div>
+  
+  )
+
 }
 
 export default App;
---
+++ src/components/CardWrapper.js
@@ -0,0 +1,9 @@
+import './CardWrapper.css';
+
+function CardWrapper(props) {
+  return (
+    <div className='outer-card'>{props.children}</div>
+  );
+}
+
+export default CardWrapper;
