c256b2967faafd19ac8a1407733b10cc5f9564ed
Njthai <naiarajudith@gmail.com>
2023-01-04 12:56:03 +0100

    start task-list

---
+++ task-list/clean-template/src/App.js
@@ -0,0 +1,19 @@
+//import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita,
+        nobis quisquam ipsum culpa nihil repudiandae consectetur illo, odit
+        libero porro exercitationem vitae consequatur eaque labore excepturi.
+        Aliquid dolores harum inventore itaque, alias hic. Modi eaque aperiam
+        maxime deserunt adipisci omnis porro quod architecto molestiae, quasi
+        quaerat vero alias pariatur dolore!
+      </p>
+    </div>
+  );
+}
+
+export default App;
